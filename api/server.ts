e75394ed14fa67ed3ed9183f33d770adb0647e3c
Danilo Maia <dandbass2@gmail.com>
2024-12-26 20:36:49 -0500

    updates

--- api/server.ts
+++ api/server.ts
@@ -1,14 +1,22 @@
 import Fastify from "fastify"
 import cors from "@fastify/cors"
 import dotenv from "dotenv"
-import { routes } from "./routes"
+import { routes } from "../routes/routes"
 
-const app = Fastify({ logger: true })
 dotenv.config()
 
+const app = Fastify({ logger: true })
+
+// Define o handler para erros globais
 app.setErrorHandler((error, request, reply) => {
   reply.code(400).send({ message: error.message })
 })
+
+// Função serverless para o Vercel
+export default async function handler(req: any, res: any) {
+  await app.ready() // Aguarda o servidor estar pronto
+  app.server.emit("request", req, res) // Passa a requisição e resposta do Vercel para o Fastify
+}
 
 const start = async () => {
   app.register(cors)
